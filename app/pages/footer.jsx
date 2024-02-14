4c7e500a118ddf2a3d8a1b979b3307a42c0a49e0
dodi <dodisuta0@gmail.com>
2024-02-14 07:56:23 +0700

    fix resposive

--- app/pages/footer.jsx
+++ app/pages/footer.jsx
@@ -4,17 +4,17 @@ import React from "react";
 const Footer = () => {
   return (
     <div id="contact" className="w-full flex justify-center items-center">
-      <div className="w-[60%] h-[50%] flex flex-col justify-start items-center">
+      <div className="sm:w-[60%] w-full sm:px-0  px-12 sm:h-[50%] flex flex-col justify-cneter items-center">
         <div className="w-full flex items-center justify-start">
           <div className="flex flex-col mb-12 ">
             <h1 className="text-blue-600 font-bold">CONTACT</h1>
             <p className="font-bold text-base">Don't bee shy! Hit me up!👇</p>
           </div>
         </div>
-        <div className="flex w-full gap-[100px] items-center mb-12">
+        <div className="flex w-full sm:justify-start justify-between  items-center mb-12">
           <div className="flex justify-center items-center">
             <Image className="w-[50px] h-[50px]" src={"/map.svg"} width={50} height={50} alt="email" />
-            <div>
+            <div className="w-full">
               <h1 className="font-bold">Location</h1>
               <p className="text-sm">Jawabarat, Indonesia</p>
             </div>
